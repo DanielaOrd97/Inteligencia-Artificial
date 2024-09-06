7f03ce6cac9094e9e1025fdeb3766176f486824f
DanielaOrd97 <201G0272@rcarbonifera.tecnm.mx>
2024-09-06 10:12:34 -0600

    Direcciones

--- Laberinto A Estrella/AEstrella.cs
+++ Laberinto A Estrella/AEstrella.cs
@@ -33,5 +33,61 @@ namespace Laberinto_A_Estrella
         //Calcular distancia Manhattan de un punto a otro con valores absolutos.
         public int Heuristica(Nodo a, Nodo b) => 
             Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
+
+        public List<Nodo> GenerarSucesores(Nodo nodo)
+        {
+            List<Nodo> sucesores = new List<Nodo>();
+            int nuevaX;
+            int nuevaY;
+
+            //Hacia arriba
+            nuevaX = nodo.X;
+            nuevaY = nodo.Y-1;    
+
+            if(nuevaX >= 0)
+            {
+                sucesores.Add(_nodos[nuevaX,nuevaY]);
+            }
+
+			//Hacia abajo
+			nuevaX = nodo.X;
+			nuevaY = nodo.Y + 1;
+
+            if(nuevaY < _alto)
+            {
+                sucesores.Add(_nodos[nuevaX, nuevaY]);
+            }
+
+            //Hacia la izq
+            nuevaX = nodo.X - 1;
+            nuevaY = nodo.Y;
+
+            if(nuevaX >= 0)
+            {
+                sucesores.Add(_nodos[nuevaX, nuevaY]);
+            }
+
+            //Hacia la derecha
+            nuevaX = nodo.X + 1;
+            nuevaY = nodo.Y;
+
+            if(nuevaX < _ancho)
+            {
+                sucesores.Add(_nodos[nuevaX, nuevaY]);
+            }
+
+            return sucesores;
+        }
+
+        public List<Nodo> BuscarRuta(Nodo origen, Nodo meta)
+        {
+            List<Nodo> abiertos = new List<Nodo>();
+            abiertos.Add(origen); //Iniciar algoritmo.
+            HashSet<Nodo> cerrados = new HashSet<Nodo>();
+            //origen.G: Costo de la distancia.
+            origen.G = 0; //distancia a la que esta el origen del origen.
+            origen.H = Heuristica(origen, meta);
+
+        }
     }
 }
